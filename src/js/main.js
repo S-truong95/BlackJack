728cbd3b57216492b9e462ee7632587e2c0b8303
Steven <steventruong1995@yahoo.com>
2019-10-24 15:57:52 -0400

    add user, dealer, table

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
 import BlackJack from "blackjack-dealer-logic"
 
 export default () => {
+  alert("Lets play some blackjack")
   const app = document.getElementById("app")
+  const user = new BlackJack.User()
+  const dealer = new BlackJack.Dealer()
+  const table = new BlackJack.Table(user, dealer)
   const blackjack = new BlackJack(app)
+  const hitButton = document.getElementById("btn-hit")
+  const standButton = document.getElementById("btn-stand")
+  const doubleButton = document.getElementById("btn-double")
+
+
+    hitButton.onclick = function(){}
+    standButton.onclick = function(){}
+    doubleButton.onclick = function(){}
+    
+
+
+
+
   blackjack.start()
 }
