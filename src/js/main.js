01b691b1782277ee31035acb96a22ad6e2b95c0f
Steven <steventruong1995@yahoo.com>
2019-10-24 15:29:13 -0400

    add blackjack dealer logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+import BlackJack from "blackjack-dealer-logic"
+
 export default () => {
-    alert("it works!")
-  }
+  const app = document.getElementById("app")
+  const blackjack = new BlackJack(app)
+  blackjack.start()
+}
