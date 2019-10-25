58bb49f795f3e82d3bd944bd880e2194958d3f2e
Steven <steventruong1995@yahoo.com>
2019-10-25 14:57:44 -0400

    got html to populate

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,58 @@
 import BlackJack from "blackjack-dealer-logic"
 
+
 export default () => {
   alert("Lets play some blackjack")
-  const app = document.getElementById("app")
-  const user = new BlackJack.User()
-  const dealer = new BlackJack.Dealer()
-  const table = new BlackJack.Table(user, dealer)
-  const blackjack = new BlackJack(app)
+  const game = BlackJack.singleDeckGame
+  const playButton = document.getElementById("btn-play")
   const hitButton = document.getElementById("btn-hit")
   const standButton = document.getElementById("btn-stand")
   const doubleButton = document.getElementById("btn-double")
+  
+
+ 
+    
+  
+    playButton.onclick = function(){
+        
+            document.getElementById("start")
+            start.innerHTML = `Your current chip count is: ${game.getUserChips()}`
+            
+            const wager = window.prompt("Ante");
+            game.receiveAnte(wager);
+
+            document.getElementById("play")
+            play.innerHTML = `Your bet is: ${wager}`
+            
+            game.deal();
+            
+            document.getElementById("dealer")
+            dealer.innerHTML = `Dealer hand: ${game.getDealerCardUp()}`
+            
+            document.getElementById("player")
+            player.innerHTML = `Your hand: ${game.getUserHandValue()}`
 
 
-    hitButton.onclick = function(){}
-    standButton.onclick = function(){}
-    doubleButton.onclick = function(){}
+        
+            dealButton.onclick = function(){
+                table.deal()
+            }
+            hitButton.onclick = function(){
+                table.hitUser()
+            }
+            standButton.onclick = function(){
+                table.standUser()
+            }
+            doubleButton.onclick = function(){
+                table.doubleUser()
+            }
+    }
+
+
     
 
 
 
 
-  blackjack.start()
+ 
 }
